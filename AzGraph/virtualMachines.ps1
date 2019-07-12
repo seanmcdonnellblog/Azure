0d83219b6bb46bbc386dadedd77a779174f3f1de
Sean McDonnell <contact@seanmcdonnell.blog>
2019-07-12 15:40:56 +0100

    Update virtualMachines.ps1

--- AzGraph/virtualMachines.ps1
+++ AzGraph/virtualMachines.ps1
@@ -1,36 +1,36 @@
-# Display Virtual Machines Name, Location, Resource Group Name and Tags
+# Global Variables
 $resourceType = 'microsoft.compute/virtualMachines'
-$query = "where type =~ '$resourceType' | project name, location, resourceGroup, tags"
+$vmSize = 'properties.hardwareProfile.vmSize'
+$osProfile = 'properties.storageProfile.osDisk.osType'
+$imageOffer = 'properties.storageProfile.imageReference.offer'
+
+# Display Virtual Machines Name, Location, Resource Group Name and Tags
+$query = "where type =~ '$resourceType' | project name,resourceGroup, location, size = $vmSize, License = properties.licenseType, subscriptionId, type, os = $osProfile, tags"
 $vm= Search-AzGraph -Query $query
 $vm
 
 # Display Virtual Machine count per location
-$resourceType = 'microsoft.compute/virtualMachines'
 $query = "where type =~ '$resourceType' | summarize count() by location"
 $vmCount= Search-AzGraph -Query $query
 $vmCount
 
 # Display Count of VM Sizes
-$resourceType = 'microsoft.compute/virtualMachines'
-$query = "where type =~ '$resourceType' | project  SKU = tostring(properties.hardwareProfile.vmSize)| summarize count() by SKU"
+$query = "where type =~ '$resourceType' | project  SKU = tostring($vmSize)| summarize count() by SKU"
 $vmCountbySKU= Search-AzGraph -Query $query
 $vmCountbySKU 
 
 # Count of Image SKU via location
-$resourceType = 'microsoft.compute/virtualMachines'
-$query = "where type =~ '$resourceType' | summarize count() by tostring(properties.storageProfile.imageReference.offer), location"
+$query = "where type =~ '$resourceType' | summarize count() by tostring($imageOffer), location"
 $vmCount= Search-AzGraph -Query $query
 $vmCount
 
 ## List VMs that do not have Automatic Updates enabled
-$resourceType = 'microsoft.compute/virtualMachines'
 $query = "where type =~ '$resourceType' and properties.osProfile.windowsConfiguration.enableAutomaticUpdates == 'false' | summarize count() by location"
 $vmCount= Search-AzGraph -Query $query
 $vmCount
 
 # Search for a specific VM
 $vmName = 'UKS'
-$resourceType = 'microsoft.compute/virtualMachines'
 $query = "where type =~ 'microsoft.compute/virtualmachines' and name matches regex @'^$vmName(.*)[0-9]+$' | project name, resourceGroup, location, tags | order by name asc"
 $searchVM = Search-AzGraph -Query $query
 $searchVM
