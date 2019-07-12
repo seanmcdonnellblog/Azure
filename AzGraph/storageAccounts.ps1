123845a009c21b7487ea21011d87d977f68a8d49
Sean McDonnell <contact@seanmcdonnell.blog>
2019-07-12 15:42:07 +0100

    Update storageAccounts.ps1

--- AzGraph/storageAccounts.ps1
+++ AzGraph/storageAccounts.ps1
@@ -1,35 +1,27 @@
-Install-Module -Name Az.ResourceGraph
-Get-Command -Module 'Az.ResourceGraph
-
-## Help commands
-**Get-Help Search-AzGraph -Examples**
+# Global Variables
+$resourceType = 'microsoft.storage/storageAccounts'
 
 ## Find tags for Storage Accounts
-$resourceType = 'microsoft.storage/storageAccounts'
 $query = "where type =~ '$resourceType' | project name, resourceGroup, type, location, tags"
 $storageTags = Search-AzGraph -Query $query
 $storageTags
 
 ## Display Count for the Service Tag
-$resourceType = 'microsoft.storage/storageAccounts'
 $query = "where type =~ '$resourceType' | project tag = tostring(tags.Service) | summarize count() by tag"
 $storageTags = Search-AzGraph -Query $query
 $storageTags
 
 ## Count count of Storage Accounts by location
-$resourceType = 'microsoft.storage/storageAccounts'
 $query =  "where type =~ '$resourceType' | summarize count() by location"
 $storageAccountCount = Search-AzGraph -Query $query
 $storageAccountCount
 
 ## Find any Storage accounts that are not VNET integrated (Accessible from Internet)
-$resourceType = 'microsoft.storage/storageAccounts'
 $query =  "where type =~ '$resourceType' and aliases['Microsoft.Storage/storageAccounts/networkAcls.defaultAction'] == 'Allow' | project name, kind, location"
 $storagePuplicAccess = Search-AzGraph -Query $query
 $storagePuplicAccess
 
 ## Find any storage accounts which are not located in Primary Loation UK South
-$resourceType = 'microsoft.storage/storageAccounts'
 $query = "where type =~ '$resourceType' and aliases['Microsoft.Storage/storageAccounts/primaryLocation'] != 'uksouth' | project name, location, resourceGroup"
 $storageLocation = Search-AzGraph -Query $query
 $storageLocation
