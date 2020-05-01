af130c62843bb7f92221b64eabb231c9631bd4f0
Sean McDonnell <contact@seanmcdonnell.blog>
2020-05-01 11:56:00 +0100

    Update Get-AzurePolicyDefinition.ps1

--- PowerShell/Get-AzurePolicyDefinition.ps1
+++ PowerShell/Get-AzurePolicyDefinition.ps1
@@ -1,6 +1,10 @@
+# Create an empty Hash Table
 $policy = @()
+
+# Get All Policy Definitions
 $policies = Get-AzPolicyDefinition
 
+# Polulate hash table with Policy Display Name, Description, Category, ID and Type
 $policy += $policies | select @{Name="Display Name";expression={$_.Properties.displayName}},`
                             @{Name="Description";expression={$_.Properties.description}},`
                             @{Name="Category";expression={$_.Properties.metadata.category}},`
