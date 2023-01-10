5de8fa886e0b7826975c366a1d6e0e50b5d8c647
Sean McDonnell <contact@seanmcdonnell.blog>
2023-01-10 12:22:53 +0000

    Update Create-RG-StorageAcc.ps1

--- PowerShell/Storage/Create-RG-StorageAcc.ps1
+++ PowerShell/Storage/Create-RG-StorageAcc.ps1
@@ -1,59 +1,40 @@
 [CmdletBinding()]
-        Param(
-        	[Parameter(Mandatory=$true, ValueFromPipeline=$true)]
-        	[String] $location,
+Param(
+    [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+    [String] $location,
+    [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+    [string] $resourceGroup,
+    [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+    [string] $storAccountName,
+    [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+    [string] $storSKU,
+    [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+    [string] $storType
+)
 
-	    	[Parameter(Mandatory=$true, ValueFromPipeline=$true)]
-	    	[string] $resourceGroup,
- 
-	    	[Parameter(Mandatory=$true, ValueFromPipeline=$true)]
-	    	[string] $storAccountName,
+$existRG = Get-AzResourceGroup -ResourceGroupName $resourceGroup -ErrorAction SilentlyContinue
+$existSA = Get-AzStorageAccount -ResourceGroupName $resourceGroup -Name $storAccountName -ErrorAction SilentlyContinue
 
-          	[Parameter(Mandatory=$true, ValueFromPipeline=$true)]
-	    	[string] $storSKU,
-
-          	[Parameter(Mandatory=$true, ValueFromPipeline=$true)]
-	    	[string] $storType
-    	)
-
-$rgName = Get-AzResourceGroup -ResourceGroupName $resourceGroup -ErrorAction SilentlyContinue
-
-  if ($rgName) {
-        Write-Host "Resource Group: $resourceGroup Already Exists"
-    } else {
-        Write-Host "Creating Resource Group: $resourceGroup"
-        # Create new Resource Group
-        $newRG = New-AzResourceGroup -Name $resourceGroupName -Location $location -Force -ErrorAction Stop
-        
-        # Check if the creation was successful
-        if ($newRG.ProvisioningState -eq "Succeeded") {
-            Write-Host "Resource Group: $resourceGroup created successfully"
-        } else {
-            Write-Host $newRG
-            Write-Host "Resource Group: $resourceGroup creation failed. Terminating"
-            Exit
-        }
+if (!$existRG) {
+    Write-Verbose "Creating Resource Group: $resourceGroup"
+    $newRG = New-AzResourceGroup -Name $resourceGroup -Location $location -Force -ErrorAction Stop
+    if (!($newRG.ProvisioningState -eq "Succeeded")) {
+        Write-Verbose "Resource Group: $resourceGroup creation failed. Terminating"
+        Exit
     }
+    Write-Verbose "Resource Group: $resourceGroup created successfully"
+} else {
+    Write-Verbose "Resource Group: $resourceGroup Already Exists"
+}
 
-    $sa = Get-AzStorageAccount -ResourceGroupName $resourceGroup -Name $storAccountName -ErrorAction SilentlyContinue
-
-    if ($sa) {
-        Write-Host "Storage Account: $storAccountName Already Exists"
-    } else {
-        Write-Host "Creating Storage Account: $storAccountName"
-
-        $newSA = New-AzStorageAccount -Name $storAccountName `
-            -Location $location `
-            -SkuName $storSKU `
-            -Kind $storType `
-            -ResourceGroupName $resourceGroup -ErrorAction Stop
-
-        # Check if the creation was successful
-        if ($newSA.ProvisioningState -eq "Succeeded") {
-            Write-Host "Storage Account: $storAccountName created successfully"
-        } else {
-            Write-Host $newSA
-            Write-Host "Storage Account: $storAccountName creation failed. Terminating"
-            Exit
-        }
+if (!$existSA) {
+    Write-Verbose "Creating Storage Account: $storAccountName"
+    $newSA = New-AzStorageAccount -Name $storAccountName -Location $location -SkuName $storSKU -Kind $storType -ResourceGroupName $resourceGroup -Force -ErrorAction Stop
+    if (!($newSA.ProvisioningState -eq "Succeeded")) {
+        Write-Verbose "Storage Account: $storAccountName creation failed. Terminating"
+        Exit
     }
+    Write-Verbose "Storage Account: $storAccountName created successfully"
+} else {
+    Write-Verbose "Storage Account: $storAccountName Already Exists"
+}
