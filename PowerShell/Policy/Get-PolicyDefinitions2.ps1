afe4adfa2dbe252ea86b2ab9bfb66d01a9013a04
Sean McDonnell <contact@seanmcdonnell.blog>
2023-02-01 15:56:08 +0000

    Update Get-PolicyDefinitions2.ps1

--- PowerShell/Policy/Get-PolicyDefinitions2.ps1
+++ PowerShell/Policy/Get-PolicyDefinitions2.ps1
@@ -1,25 +1,16 @@
-$policy = @()
-$categories = @()
-$category = ""
-
-# Gets all definition
 $policies = Get-AzPolicyDefinition -Builtin
-$categories = $policies.Properties.metadata.category | Sort -Unique
-Foreach($category in $categories){
-    $polizy = $policies | Where{$_.properties.metadata.category -eq $category}
+$categories = ($policies.Properties.metadata.category | Sort-Unique)
 
-    # Populates the hashtable with custom fields
-    $policy+=$polizy | select @{Name="Display Name";expression={$_.Properties.displayName}},`
-                     @{Name="Allowed Values";expression={[string]::Join(", ",($_.Properties.Parameters.effect.allowedValues.split(" ")))}},`
-                     @{Name="Description";expression={$_.Properties.description}},`
-                     @{Name="Policy ID";expression={$_.Name}},`
-                     @{Name="Policy Type";expression={$_.Properties.policyType}},`                     
-                     @{Name="Category";expression={$_.Properties.metadata.category}}
-
-    $policy | Export-Excel -Path ".\AzurePolicy-Builtin-Policies.xlsx" -AutoSize -TableStyle Medium16 -WorksheetName $category -ConditionalText $(
-                New-ConditionalText Deprecated DarkRed LightPink
-                New-ConditionalText Preview Blue Cyan
-            )
-    $policy = @()
-    $polizy = @()
+Foreach ($category in $categories) {
+    $polizy = $policies | Where-Object { $_.properties.metadata.category -eq $category }
+    $policy = $polizy | Select-Object 'Display Name' = $_.Properties.displayName,
+                                      'Allowed Values' = [string]::Join(', ', ($_.Properties.Parameters.effect.allowedValues.split(' '))),
+                                      'Description' = $_.Properties.description,
+                                      'Policy ID' = $_.Name,
+                                      'Policy Type' = $_.Properties.policyType,
+                                      'Category' = $_.Properties.metadata.category
+    $policy | Export-Excel -Path '.\AzurePolicy-Builtin-Policies.xlsx' -AutoSize -TableStyle Medium16 -WorksheetName $category -ConditionalText (
+        New-ConditionalText Deprecated DarkRed LightPink,
+        New-ConditionalText Preview Blue Cyan
+    )
 }
